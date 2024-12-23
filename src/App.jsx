e5acad483b49d5f18edab9e0786a948a71d5a01b
sohag <shishirsohagislam@gmail.com>
2024-12-23 22:11:39 +0600

    square made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-export default function App() {
+function Square({ value }) {
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  return <button onClick={handleClick} className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+}
+
+export default function Board() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello Tic Tac Toe</h1>
+    <>
+      <div>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div>
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div>
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
   );
 }
